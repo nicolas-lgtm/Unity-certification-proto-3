1f5a028a83e7da137bafcd7a9601bcbcd361c10a
Ernouf <48248463+Ernouf@users.noreply.github.com>
2020-04-03 22:30:49 +0200

    init commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float jumpForce;
+    public float gravityModifier;
+    public bool isOnGround = false;
+    public bool gameOver = false;
+
+    private Animator playerAnim;
+    private Rigidbody playerRb;
+
+    private AudioSource audioSource;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+
+ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
+        {
+            Jump();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            if(!gameOver) dirtParticle.Play();
+            isOnGround = true;
+        } 
+        else if (collision.gameObject.CompareTag("Obstacle") && !gameOver)
+        {
+            GameOver();
+        }
+    }
+
+    private void Jump()
+    {
+        audioSource.PlayOneShot(jumpSound, 1f);
+        playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        isOnGround = false;
+        playerAnim.SetTrigger("Jump_trig");
+        dirtParticle.Stop();
+    }
+
+    private void GameOver()
+    {
+        audioSource.PlayOneShot(crashSound, 1f);
+        explosionParticle.Play();
+        dirtParticle.Stop();
+        gameOver = true;
+        int randAnim = Random.Range(1, 3);
+        playerAnim.SetBool("Death_b", true);
+        playerAnim.SetInteger("DeathType_int", randAnim);
+    }
+}
